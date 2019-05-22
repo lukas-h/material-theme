241310861ab6fceed238e219e029565d1b755171
NishkaDG <nishka.dasgupta@yahoo.com>
2019-05-22 15:45:49 +0530

    Update index.js

--- styles/index.js
+++ styles/index.js
@@ -1,10 +1,14 @@
 var hide_lists = function(cb) {
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
+	$('#cv').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
-    $('#projects-btn').removeClass('disabled')
+    $('#projects-btn').removeClass('disabled');
+	$('#cv-btn').removeClass('disabled')
 };
 var show_projects = function() {
+	$('#cv-btn').removeClass('disabled');
+	$('#cv').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300, function() {
         $('#projects').fadeIn(300)
@@ -12,9 +16,21 @@ var show_projects = function() {
     $('#projects-btn').addClass('disabled')
 };
 var show_posts = function() {
+	$('#cv-btn').removeClass('disabled');
+	$('#cv').fadeOut(300);
     $('#projects-btn').removeClass('disabled');
     $('#projects').fadeOut(function() {
         $('#posts').fadeIn(300)
     });
     $('#posts-btn').addClass('disabled')
 };
+var show_cv = function() {
+    $('#posts-btn').removeClass('disabled');
+    $('#posts').fadeOut(300);
+	$('#projects-btn').removeClass('disabled');
+    $('#projects').fadeOut(function() {
+        $('#cv').fadeIn(300)
+    });
+    $('#cv-btn').addClass('disabled')
+};
+	
