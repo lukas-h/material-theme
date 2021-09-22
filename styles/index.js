9a3b3469655b20343876b7fbb4a2c6d9900de8ff
Nishka Dasgupta <nishka.dasgupta@yahoo.com>
2021-09-22 11:27:20 +0200

    Update index.js

--- styles/index.js
+++ styles/index.js
@@ -2,13 +2,17 @@ var hide_lists = function(cb) {
     $('#posts').fadeOut(300);
     $('#projects').fadeOut(300);
 	$('#cv').fadeOut(300);
+	$('#papers').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
     $('#projects-btn').removeClass('disabled');
-	$('#cv-btn').removeClass('disabled')
+	$('#cv-btn').removeClass('disabled');
+	$('#papers-btn').removeClass('disabled')
 };
 var show_projects = function() {
 	$('#cv-btn').removeClass('disabled');
 	$('#cv').fadeOut(300);
+	$('papers-btn').removeClass('disabled');
+	$('#papers').fadeOut(300);
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300, function() {
         $('#projects').fadeIn(300)
@@ -18,6 +22,8 @@ var show_projects = function() {
 var show_posts = function() {
 	$('#cv-btn').removeClass('disabled');
 	$('#cv').fadeOut(300);
+	$('papers-btn').removeClass('disabled');
+	$('#papers').fadeOut(300);
     $('#projects-btn').removeClass('disabled');
     $('#projects').fadeOut(function() {
         $('#posts').fadeIn(300)
@@ -27,10 +33,21 @@ var show_posts = function() {
 var show_cv = function() {
     $('#posts-btn').removeClass('disabled');
     $('#posts').fadeOut(300);
+	$('papers-btn').removeClass('disabled');
+	$('#papers').fadeOut(300);
 	$('#projects-btn').removeClass('disabled');
     $('#projects').fadeOut(function() {
         $('#cv').fadeIn(300)
     });
     $('#cv-btn').addClass('disabled')
+};
+var show_papers = function() {
+    $('#posts-btn').removeClass('disabled');
+    $('#posts').fadeOut(300);
+	$('#projects-btn').removeClass('disabled');
+    $('#projects').fadeOut(function() {
+        $('#papers').fadeIn(300)
+    });
+    $('#papers-btn').addClass('disabled')
 };
 	
